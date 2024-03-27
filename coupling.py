4da661e9e4d65e9c487d79f516bb8737241f5b51
M-Kunkle <101233244+M-Kunkle@users.noreply.github.com>
2024-03-27 13:42:28 -0500

    post wednesday testing

--- coupling.py
+++ coupling.py
@@ -27,7 +27,7 @@ AMPLITUDE = .5 # 250mV
 WFM_TYPE = 2
 PULSE_WIDTH = 10e-9
 SIGNAL_SAMPLES = 5000
-START_FILT = .7e9
+START_FILT = .9e9
 STOP_FILT = 2e9
 
 ''' Device IDs: '''
@@ -50,10 +50,12 @@ interface.vertical_scale_reset(scope, ch=3, ycenter=0, ydiv=0.2, yoffset=0)
 
 ''' Upload Excitation Signals '''
 wfm_num = input('Enter Waveform Number: ')
-tf_fn = 'transferfunction' + wfm_num + '.csv'
+# tf_fn = 'transferfunction' + wfm_num + '.csv'
+tf_fn = 'hec_last.csv'
 nb_fn = 'narrowband' + wfm_num + '.csv'
 tft,tf_sig = ut.read_cst_csv(tfdir / tf_fn,1)
-nbt,nb_sig = ut.read_cst_csv(tfdir / nb_fn,1)
+# tft = np.loadtxt(tfdir / tf_fn)
+# nbt,nb_sig = ut.read_cst_csv(tfdir / nb_fn,1)
 
 ''' Transfer Function Shot'''
 interface.awg_upload_wfm(awg, np.asarray(tf_sig), .5, FS, 'current_iter.seqx')
@@ -69,29 +71,29 @@ filt_tftem = sigs.wfm_filter(tf_time, np.squeeze(tf_TEM_wfm), .7e9, 2e9)
 ut.signal_plot(tf_time,np.squeeze(filt_tfdut),0,3e9,unit='V', titlestr='TF DUT ')
 ut.signal_plot(tf_time,np.squeeze(filt_tftem),0,3E9,unit='V', titlestr='TF TEM ')
 
-''' Narrowband Shot'''
-interface.capture_single_sequence(scope)
-interface.awg_upload_wfm(awg, np.asarray(nb_sig), .5, FS, 'current_iter.seqx')
-interface.awg_fire(awg)
-time.sleep(1)
+# ''' Narrowband Shot'''
+# interface.capture_single_sequence(scope)
+# interface.awg_upload_wfm(awg, np.asarray(nb_sig), .5, FS, 'current_iter.seqx')
+# interface.awg_fire(awg)
+# time.sleep(1)
 
-nb_time, nb_DUT_wfm,_ = interface.get_waveform_from_scope(scope, ch=1)
-_, nb_TEM_wfm,_ = interface.get_waveform_from_scope(scope, ch=2)
+# nb_time, nb_DUT_wfm,_ = interface.get_waveform_from_scope(scope, ch=1)
+# _, nb_TEM_wfm,_ = interface.get_waveform_from_scope(scope, ch=2)
 
-filt_nbdut = sigs.wfm_filter(nb_time, np.squeeze(nb_DUT_wfm), .7e9, 2e9)
-filt_nbtem = sigs.wfm_filter(nb_time, np.squeeze(nb_TEM_wfm), .7e9, 2e9)
-ut.signal_plot(nb_time,np.squeeze(filt_nbdut),0,3e9,unit='V', titlestr='NB DUT ')
-ut.signal_plot(nb_time,np.squeeze(filt_nbtem),0,3E9,unit='V', titlestr='NB TEM ')
+# filt_nbdut = sigs.wfm_filter(nb_time, np.squeeze(nb_DUT_wfm), .7e9, 2e9)
+# filt_nbtem = sigs.wfm_filter(nb_time, np.squeeze(nb_TEM_wfm), .7e9, 2e9)
+# ut.signal_plot(nb_time,np.squeeze(filt_nbdut),0,3e9,unit='V', titlestr='NB DUT ')
+# ut.signal_plot(nb_time,np.squeeze(filt_nbtem),0,3E9,unit='V', titlestr='NB TEM ')
 
 ''' Create File Names'''
 dut_num = input('Enter DUT Number: ')
-fn1 = 'DUT' + dut_num + '_WC' + wfm_num + '.csv'
-fn2 = 'DUT' + dut_num + '_NB' +  wfm_num + '.csv'
+fn1 = 'DUT' + dut_num + '_' + wfm_num + '.csv'
+# fn2 = 'DUT' + dut_num + '_NB' +  wfm_num + '.csv'
 
 ''' Save Data '''
 csv_header = ','.join(('Time (s)','DUT (V)', 'TEM (V)'))
 ut.array_to_csv(coupdir / fn1, csv_header, tf_time, filt_tfdut.real, filt_tftem.real)
-ut.array_to_csv(coupdir / fn2, csv_header, nb_time, filt_nbtem.real, filt_nbtem.real)
+# ut.array_to_csv(coupdir / fn2, csv_header, nb_time, filt_nbtem.real, filt_nbtem.real)
 
 ''' Clear Instruments'''
 awg.close()
